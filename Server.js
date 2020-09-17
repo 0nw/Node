b07bbd7c1b6afb63af72aaf64a31bb2700b1830e
Finn Reynolds <46507049+finnr20@users.noreply.github.com>
2020-09-17 10:00:24 -0400

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,9 +1,6 @@
 var app = require('express')();
 var http = require('http').createServer(app);
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
+const router = require("./Router.js")
 
 http.listen(3000, () => {
   console.log('listening on *:3000');
