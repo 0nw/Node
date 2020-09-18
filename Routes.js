d1831e198b48cedaeebc929fc97fb37a2513f7b8
Finn Reynolds <46507049+finnr20@users.noreply.github.com>
2020-09-18 08:43:27 -0400

    Update Routes.js

--- Routes.js
+++ Routes.js
@@ -1,5 +1,4 @@
 var app = require('express')();
-const config = require("./data/config.json")
 app.set('view engine', 'pug')
 app.get('/', function (req, res) {
   res.render('index', { title: "Title"})
