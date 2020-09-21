9a356de8a995f59fb65dae2ffa3a4f50bdea4b67
Finn Reynolds <46507049+finnr20@users.noreply.github.com>
2020-09-21 07:27:05 -0400

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 app.use(require("./Auth.js"));
-app.use(require("./Chat.js"));
+app.use(require("./Post.js"));
 app.use(require("./Routes.js"));
 http.listen(3000, () => {
   console.log('Listening on port:3000');
