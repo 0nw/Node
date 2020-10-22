784656fefc6cee1a09849693f2056280f5b38bb0
Finn Reynolds <46507049+finnr20@users.noreply.github.com>
2020-10-22 07:37:05 -0400

    Update Auth.js

--- Auth.js
+++ Auth.js
@@ -1,9 +1,18 @@
 var app = require('express')();
 const users = require("./data/users.json")
 console.log("Auth.js Loaded!")
-function checkExist(){
+function checkExist(username){
   var exists = None;
-  
+  x = 0;
+  taken = []
+  for(x in range(len(users))){
+    taken.append(users[x].username)
+    x++
+  }
+  if(username in taken){
+    exists = true;
 }
+}
+
 module.exports = app;
 //\\
